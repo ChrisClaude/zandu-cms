ef0a1d5196f07278ea433d83b3bd3df8e8d3f36a
Claude De-Tchambila <claude.detchambila@dariel.co.za>
2021-11-05 20:49:55 +0200

    updated content type page

--- pages/contenttypes/[name].tsx
+++ pages/contenttypes/[name].tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import { useRouter } from 'next/router';
+import AddIcon from '@mui/icons-material/Add';
 import Meta from '@/components/Meta';
 import NavBar from '@/components/NavBar';
 import ProfileMenuItem from '@/components/ProfileMenuItem';
 import Layout from '@/components/Layout';
+import { capitalizeFirstLetter } from '@/helpers/index';
+import Button from '@mui/material/Button';
 
 const ContentTypes = () => {
 	const router = useRouter();
@@ -15,10 +18,15 @@ const ContentTypes = () => {
 				<ProfileMenuItem />
 			</NavBar>
 			<div className='px-4 py-6'>
-				<div>
+				<div className="flex items-center">
 					<div>
-						<h2>{name}</h2>
-						<div>0 entries found</div>
+						<h2>{name && capitalizeFirstLetter(name.toString())}</h2>
+						<p className="text-sm text-gray-500">0 entries found</p>
+					</div>
+					<div className='ml-auto'>
+						<Button variant="contained" startIcon={<AddIcon />}>
+							Add New {name}
+						</Button>
 					</div>
 				</div>
 			</div>
