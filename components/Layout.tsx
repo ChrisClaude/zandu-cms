713120eb48bdbeb676be1bea35d2bcf16318fffa
Claude De-Tchambila <claude.detchambila@dariel.co.za>
2021-11-05 15:20:52 +0200

    added sidebar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,6 +4,7 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Header from './Header';
 import Footer from './Footer';
+import SideBar from '@/components/layout/SideBar';
 
 
 type LayoutProp = {
@@ -20,8 +21,7 @@ const Layout = ({ children }: LayoutProp) => (<>
 	<Grid container className="h-screen">
 		<Grid item xs={2} className="h-full">
 			<Item className="h-full overflow-auto flex flex-col bg-gray-800 rounded-none">
-				<Header />
-				<Footer className="mt-auto" />
+				<SideBar/>
 			</Item>
 		</Grid>
 		<Grid item xs={10} className="h-full">
---
+++ components/layout/SideBarMenu/SideBarMenu.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import PeopleIcon from '@mui/icons-material/People';
+import SearchIcon from '@mui/icons-material/Search';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { default as cn } from 'classNames';
+
+type SideBarProps = React.HTMLProps<HTMLDivElement>;
+
+const SideBarMenu = (props: SideBarProps) => <aside {...props} className={cn('text-gray-100', props.className)}>
+	<List>
+		<ListItem disablePadding>
+			<ListItemButton>
+				<ListItemText primary='Collection Types' />
+				<ListItemIcon>
+					<SearchIcon className='text-gray-100' />
+				</ListItemIcon>
+			</ListItemButton>
+		</ListItem>
+		<ListItem disablePadding className='hover:border-l-2 hover:border-blue-700 transition-all'>
+			<ListItemButton>
+				<ListItemIcon>
+					<PeopleIcon className='text-gray-100' />
+				</ListItemIcon>
+				<ListItemText primary='Users' />
+			</ListItemButton>
+		</ListItem>
+	</List>
+</aside>;
+
+export default SideBarMenu;
